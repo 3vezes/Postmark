ae67f4c9717a7f8b2cd7aaa6db816fbb149f2ff8
Eric Gonzalez <eric@bypassmobile.com>
2013-12-21 21:06:17 -0600

    Wired pin activity.

--- src/main/java/com/ericrgon/nearbox/PinActivity.java
+++ src/main/java/com/ericrgon/nearbox/PinActivity.java
@@ -2,6 +2,8 @@ package com.ericrgon.nearbox;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import butterknife.Views;
 
@@ -13,15 +15,34 @@ public class PinActivity extends BaseFragmentActivity {
     public static final String NEW_PIN = "newPin";
     public static final String PIN_DATA = "pin";
 
+    private char[] pin = new char[4];
+
+    private int current = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin);
         Views.inject(this);
-        Intent intent = new Intent();
-        intent.putExtra(PinActivity.PIN_DATA,"1234");
+    }
 
-        setResult(RESULT_OK, intent);
-        finish();
+
+    public void onDigitClicked(View view) {
+        TextView digit = (TextView) view;
+
+        if(digit.getId() == R.id.back){
+            //User pressed delete.
+            current--;
+        } else {
+            pin[current] = digit.getText().charAt(0);
+            current++;
+        }
+
+        if(current == 4){
+            Intent intent = new Intent();
+            intent.putExtra(PinActivity.PIN_DATA,new String(pin));
+            setResult(RESULT_OK, intent);
+            finish();
+        }
     }
 }

94f66c6446082d283057c404ab451f8d47a7aca8
Eric Gonzalez <eric@bypassmobile.com>
2013-12-27 14:06:43 -0600

    White buttons, demo login

--- src/main/java/com/ericrgon/nearbox/LoginActivity.java
+++ src/main/java/com/ericrgon/nearbox/LoginActivity.java
@@ -158,4 +158,8 @@ public class LoginActivity extends BaseFragmentActivity {
         finish();
 
     }
+
+    public void onDemoLogin(View view) {
+        authenticate("demo","",loginCallback);
+    }
 }

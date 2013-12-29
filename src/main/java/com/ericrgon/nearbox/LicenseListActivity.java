b7f74b68295ace9de4200d16ba5083d5244d76bd
Eric Gonzalez <eric@bypassmobile.com>
2013-12-29 14:13:50 -0600

    Enable back press from license activity.

--- src/main/java/com/ericrgon/nearbox/LicenseListActivity.java
+++ src/main/java/com/ericrgon/nearbox/LicenseListActivity.java
@@ -2,6 +2,7 @@ package com.ericrgon.nearbox;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.ericrgon.nearbox.adapter.LicenseAdapter;
 import com.ericrgon.nearbox.util.Licenses;
@@ -12,5 +13,17 @@ public class LicenseListActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setListAdapter(new LicenseAdapter(Licenses.getLicenses(), this));
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
